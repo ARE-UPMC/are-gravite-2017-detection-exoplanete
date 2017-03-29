ea62487b069e612c0105ed2fe4e2bd4ecc6f6ea3
EugeneRaphanel <raphanel.eugene@gmail.com>
2017-03-29 12:11:50 +0200

    Add files via upload

--- simulation_exoplanete.py
+++ simulation_exoplanete.py
@@ -12,14 +12,14 @@ from pylab import*
 fig= plt.figure() 
 fig.set_dpi(100) 
 fig.set_size_inches(100, 100) 
-ax=plt.axes(xlim=(-0.06, 0.06), ylim=(-0.06, 0.06))
+ax=plt.axes(xlim=(-0.08, 0.08), ylim=(-0.08, 0.08))
 plt.xlabel('distance en UA')
 plt.ylabel('distance en UA')
 
 patch = plt.Circle((5,-5), 0.005, fc='b', label="HD 209458 b") 
-patch2 = plt.Circle((0, -2), 0.001, fc='r', label="centre de masse") 
-a=plt.Circle((0, -2),0.05, color="green",label=' centre de HD 209458')
-plt.annotate('masse en masse de Jupiter', xy=(0, 0), xytext=(0.003, -0.04))
+patch2 = plt.Circle((0, -2), 0.001, fc='y', label="HD 209458") 
+a=plt.Circle((0, -2),0.001, color="red",label=' centre de masse ')
+plt.annotate('masse en masse de Jupiter', xy=(0, 0), xytext=(0.003, -0.07))
 
 m=0.69
 M=102
@@ -28,22 +28,24 @@ R=0.04747
 def init(): 
      patch.center = (0, 0) 
      patch2.center = (0, 0) 
+     a.center = (0, 0)
      ax.add_patch(patch) 
      ax.add_patch(patch2) 
-     return patch, patch2, 
+     ax.add_patch(a) 
+     return a,patch, patch2, 
 
  
 def animate(t): 
      x,y = patch.center 
-     x = -R* np.cos(np.radians(t)) 
-     y =R* np.sin(np.radians(t)) 
+     x = R* np.cos(np.radians(t)) 
+     y =-R* np.sin(np.radians(t)) 
      patch.center = (x,y) 
      x,y = patch2.center 
      x =-np.cos(np.radians(t))*(m*R/(m+M)) 
-     y =+np.sin(np.radians(t))*(m *R/(m+M))
+     y =+np.sin(np.radians(t))*(m*R/(m+M)) 
      patch2.center= (x,y) 
       
-     return patch, patch2, 
+     return a,patch, patch2, 
 anim = animation.FuncAnimation(fig, animate, init_func=init,frames=360,interval=20,blit=True)
 
-plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=1, ncol=2, mode="expand", borderaxespad=0, handles=[patch, patch2,a]) 
+plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=1, ncol=2, mode="expand", borderaxespad=0, handles=[a, patch, patch2]) 
