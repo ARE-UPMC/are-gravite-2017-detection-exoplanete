efee42b54b2beef26d5c39ccf8c4ca4ec9bbb389
EugeneRaphanel <raphanel.eugene@gmail.com>
2017-03-29 09:08:44 +0200

    Add files via upload

---
+++ transit.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 28 16:20:47 2017
+
+@author: 3670463
+"""
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+from matplotlib import animation 
+from pylab import*
+fig, ax = plt.subplots() 
+fig.set_dpi(100) 
+fig.set_size_inches(10, 10) 
+ax=plt.axes(xlim=(-5, 5), ylim=(-4, 4))
+plt.xlabel('distance en UA')
+plt.ylabel('distance en UA')
+patch = plt.Circle((0,3), 0.15, fc='b', label="Exoplanete") 
+
+a= plt.Circle((0, 0), 2, fc='y', label="etoile")
+
+R=4
+def init(): 
+     patch.center = (-3, 3) 
+     
+     a.center = (0, 0)
+     
+    
+     ax.add_patch(a) 
+     ax.add_patch(patch)
+     return a,patch, 
+
+ 
+def animate(t): 
+     x,y = patch.center 
+     x = R* np.cos(np.radians(t)) -3
+     y =-R* np.sin(np.radians(t)) +3
+     patch.center = (x,y) 
+     
+      
+     return a,patch,
+     
+anim = animation.FuncAnimation(fig, animate, init_func=init,frames=360,interval=20,blit=True)
+
+plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=1, ncol=2, mode="expand", borderaxespad=0, handles=[a, patch]) 
+plt.show()
